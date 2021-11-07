ad2ee1847fa3ad682fde31f7b942fa91f2bbccda
Levi Richardson <litbelb1@outlook.com>
2021-11-06 17:45:01 -0700

    send package

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,21 @@
 import { Tedis } from "tedis";
 import * as dotenv from "dotenv";
+dotenv.config();
 const redisPort: string = process.env.REDIS_PORT || "6379";
 const db = new Tedis({
     host: process.env.REDIS_HOST,
     port: parseInt(redisPort),
     password: process.env.REDIS_PWD,
 });
+
+db.on("connect", () => {
+    console.log("DB connected");
+  });
+  db.on("timeout", () => {
+    console.log("DB timedout");
+  });
+  db.on("error", (err: Error) => {
+    console.log(`DB error: ${err}`);
+  });
 
 export { db };
--- src/routes/getpackage.ts
+++ src/routes/getpackage.ts
@@ -3,7 +3,10 @@ import { db } from "..";
 
 const getPackageRouter = async (req: express.Request, res: express.Response) => {
     const packageId = await db.get(req.params.pkgid)
-    console.log(packageId)
+    if (packageId === null) {
+        res.status(404).send("Package not found")
+    }
+    res.json({ "httpStatus": "200 OK", "package": packageId });
 }
 
 export { getPackageRouter }
